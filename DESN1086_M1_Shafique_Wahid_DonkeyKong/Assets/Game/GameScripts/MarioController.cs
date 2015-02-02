db402ddeaa89589d5b21c626abedd334de88864c
Wahid Shafique <wahidshafique@hotmail.com>
2015-02-02 17:16:50 -0500

    made some cursory changes to ladder mechanic 1
    Still need to refine it

--- DESN1086_M1_Shafique_Wahid_DonkeyKong/Assets/Game/GameScripts/MarioController.cs
+++ DESN1086_M1_Shafique_Wahid_DonkeyKong/Assets/Game/GameScripts/MarioController.cs
@@ -2,62 +2,69 @@
 using System.Collections;
 
 public class MarioController : MonoBehaviour {
-
-	// Use this for initialization
-    //bool CanMove=true; 
-	public float MaximumSpeed=10f;
-	bool MarioFacingRight=true;
 	Animator Anim;
-	
-	bool Grounded = false;
-	bool Ladder = false;
-	public Transform GroundCheck;
-	float GroundRad = 0.2f;
-	public LayerMask WhatIsGround;
-	public float JumpForce = 700f;
 
+	private bool MarioFacingRight=true;
+	private bool Grounded = false;
+	private bool Ladder = false;
+	private bool MoveCheck = true;
+	private float GroundRad = 0.2f;
+	private float Move;
+
+	public float MaximumSpeed=10f;
+	public float JumpForce = 700f;
+	public Transform GroundCheck;
+	public LayerMask WhatIsGround;
+	
+	//TODO: when ground is false then you cannot press up arbitrariliy 
 	void Start () {
 		Anim = GetComponent<Animator>();
 	}
 
 	void FixedUpdate () {
+		Move = Input.GetAxis ("Horizontal");
 
 		Grounded = Physics2D.OverlapCircle (GroundCheck.position, GroundRad, WhatIsGround);
 		Anim.SetBool ("Ground", Grounded);
 
 		Anim.SetFloat ("VSpeed", rigidbody2D.velocity.y);
 
-		 float Move;
-		//if (CanMove){
-			//CanMove=true;
-			Move = Input.GetAxis ("Horizontal");
-
+		if (MoveCheck){
 		Anim.SetFloat("Speed", Mathf.Abs(Move));
-
 		rigidbody2D.velocity = new Vector2 (Move * MaximumSpeed, rigidbody2D.velocity.y);
-		//this.transform.position.y = this.transform.position.y-0.05;
 		if (Move > 0 && !MarioFacingRight)
 			Flipper();
 			else if (Move<0 && MarioFacingRight)
 				Flipper();
 		}
+	}
 
 	void OnTriggerStay2D(Collider2D other){
 		print("hello");
-		Ladder = true;}
-
-	//var Ladder
+		//rigidbody2D.velocity = new Vector2 (0,0);
+		if (Grounded){
+		Ladder = true;
+		//MoveCheck=false;
+			Move=0;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other){
+		Ladder = false;
+		}
+	
 	void Update(){
-		if (Grounded && Input.GetKeyDown(KeyCode.Space)){
+		if (Anim.GetBool("Ladder") == false && Grounded && Input.GetKeyDown(KeyCode.Space)){
 			Anim.SetBool("Ground",false);
 			rigidbody2D.AddForce(new Vector2(0,JumpForce));
-			}
+		}
 
-		if (Ladder && Input.GetAxis ("Vertical")>0){
+		//TODO: 
+
+		if (Ladder && Input.GetAxis ("Vertical")>0 && Grounded){
 			Anim.SetBool("Ladder", Ladder);
 			transform.Translate (new Vector2(0, 0.2f)* Time.deltaTime*MaximumSpeed);
 			rigidbody2D.gravityScale=0;
-		} if (!Ladder) {
+		} else if (!Ladder) {
 			Anim.SetBool("Ladder",Ladder);
 			rigidbody2D.gravityScale=1;}
 		}
@@ -71,11 +78,6 @@ public class MarioController : MonoBehaviour {
 		TheScale.x *= -1;
 
 		transform.localScale = TheScale;
-	}
-
-
-
-
-		
+	}						
 }
  
